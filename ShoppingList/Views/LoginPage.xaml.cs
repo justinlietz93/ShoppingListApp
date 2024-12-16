704d9edd7f9e13a5e2fa5ed55cfc44892a0edbc0
Justin Lietz <jlietz93@gmail.com>
2024-12-15 20:18:24 -0800

    Synced

--- ShoppingList/Views/LoginPage.xaml.cs
+++ ShoppingList/Views/LoginPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ShoppingList.Models;
 
 namespace ShoppingList.Views;
 
@@ -14,9 +16,30 @@ public partial class LoginPage : ContentPage
         Title = "Login";
     }
 
-    private void Login_OnClicked(object sender, EventArgs e)
+    private async void Login_OnClicked(object sender, EventArgs e)
     {
-        App.SessionKey = "aaa";
+        // User info testing
+        //u: Jlietz93
+        //p: aaa
+        var data = JsonConvert.SerializeObject(new UserAccount(txtUser.Text, txtPassword.Text));
+        
+        var client = new HttpClient();
+        
+        var response = await client.PostAsync(new Uri("https://joewetzel.com/fvtc/account/login"), 
+            new StringContent(data,Encoding.UTF8,"application/json"));
+
+        var sKey = response.Content.ReadAsStringAsync().Result;
+
+        if (!string.IsNullOrEmpty(sKey) && sKey.Length < 50)
+        {
+            App.SessionKey = sKey;
+            Navigation.PopModalAsync();
+        }
+        else
+        {
+            await DisplayAlert("Error", "Invalid username or password.","Ok");
+            return;
+        }
         Navigation.PopModalAsync(); // Removes login window
     }
 
--- ShoppingList/Views/NewAccountPage.xaml.cs
+++ ShoppingList/Views/NewAccountPage.xaml.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ShoppingList.Models;
 
 namespace ShoppingList.Views;
 
@@ -14,9 +17,80 @@ public partial class NewAccountPage : ContentPage
         Title = "Create New Account";
     }
 
-    private void CreateAccount_OnClicked(object sender, EventArgs e)
+    private async void CreateAccount_OnClicked(object sender, EventArgs e)
     {
-        App.SessionKey = "aaa";
-        Navigation.PopModalAsync();
+        // Check for blank or incorrectly formatted fields
+        if (string.IsNullOrWhiteSpace(txtUser.Text) || 
+            string.IsNullOrWhiteSpace(txtPassword1.Text) || 
+            string.IsNullOrWhiteSpace(txtPassword2.Text) || 
+            string.IsNullOrWhiteSpace(txtEmail.Text))
+        {
+            await DisplayAlert("Error", "All fields must be filled in without spaces.","Ok");
+            return;
+        }
+        
+        // Check if passwords match
+        if (txtPassword1.Text != txtPassword2.Text)
+        {
+            await DisplayAlert("Error", "Passwords have to match.","Ok");
+            return;
+        }
+        
+        // Regex for validating email addresses
+        string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+        // Check for valid email address ==    xxx@xxx.xxx
+        if (!Regex.IsMatch(txtEmail.Text, emailPattern))
+        {
+            await DisplayAlert("Error", "Please enter a valid email.", "Ok");
+            return;
+        }
+        
+        // API stuff
+        var data = JsonConvert.SerializeObject(new UserAccount(txtUser.Text, txtPassword1.Text, txtEmail.Text));
+        var client = new HttpClient();
+        var response = await client.PostAsync(new Uri("https://joewetzel.com/fvtc/account/createuser"), new StringContent(data,Encoding.UTF8,"application/json"));
+
+        var AccountStatus = response.Content.ReadAsStringAsync().Result;
+        
+        
+        // Check if the user exists
+        if (AccountStatus == "user exists")
+        {
+            await DisplayAlert("Error", "This username is taken.","Ok");
+            return;
+        }
+        
+        // Check if the email is in use already
+        if (AccountStatus == "email exists")
+        {
+            await DisplayAlert("Error", "This email is taken.","Ok");
+            return;
+        }
+        
+        if (AccountStatus == "complete")
+        {
+            response = await client.PostAsync(new Uri("https://joewetzel.com/fvtc/account/createuser"), new StringContent(data,Encoding.UTF8,"application/json"));
+
+            var sKey = response.Content.ReadAsStringAsync().Result;
+
+            if (!string.IsNullOrEmpty(sKey) && sKey.Length < 50)
+            {
+                App.SessionKey = sKey;
+                Navigation.PopModalAsync();
+            }
+            else
+            {
+                await DisplayAlert("Error", "An unexpected error has occured while logging you in.","Ok");
+                return;
+            }
+        }
+        else
+        {
+            await DisplayAlert("Error", "An unexpected error has occured while trying to create your account.","Ok");
+            return;
+        }
+        
+        
+        
     }
 }
